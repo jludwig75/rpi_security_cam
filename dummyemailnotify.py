c35ccb70a6abbe86a727e87c5e942704f02dc199
Jonathan Ludwig <jr.ludwig@gmail.com>
2019-11-28 15:10:43 -0700

    fixed failed unit test

--- dummyemailnotify.py
+++ dummyemailnotify.py
@@ -7,7 +7,7 @@ class DummyEmailNotifier:
             return False
         self.notications.append({})
         return True
-    def send_recorded_video(self, movie_file_name, event_time):
+    def send_recorded_video(self, movie_file_name, event_time=None):
         if self._fail_notifications:
             return False
         self.notications.append({'file_name': movie_file_name})
