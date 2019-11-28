8f32325e704fef60c342c50dfc36bb0e393e78ac
Jonathan Ludwig <jr.ludwig@gmail.com>
2019-11-28 15:07:14 -0700

    added timestamps to email messages

--- emailnotify.py
+++ emailnotify.py
@@ -1,5 +1,6 @@
 import os
 import logging
+from datetime import datetime
 
 class EmailNotifier:
     def __init__(self, settings, mailer):
@@ -8,11 +9,13 @@ class EmailNotifier:
     def notify_motion_detected(self):
         return self._mailer.send_mail(self._settings['TO_ADDRESS'],
                     subject='Motion Detected in Garage',
-                    message='Motion was detected on camera in the garage. You should receive a video with the footage soon.')
-    def send_recorded_video(self, movie_file_name):
+                    message='Motion was detected on camera in the garage at %s. You should receive a video with the footage soon.' % str(datetime.now()))
+    def send_recorded_video(self, movie_file_name, event_time=None):
+        if not event_time:
+            event_time = datetime.now()
         if self._mailer.send_mail_with_attached_file(self._settings['TO_ADDRESS'],
                     subject='Motion Video Captured in Garage',
-                    message='Motion was recorded on camera in the garage. The video is attached.',
+                    message='Motion was recorded on camera in the garage at %s. The video is attached.' % str(event_time),
                     file_name=movie_file_name):
             logging.info('Video file %s successfully sent. Deleting video file form disk' % movie_file_name)
             try:
--- videoupdater.py
+++ videoupdater.py
@@ -1,6 +1,7 @@
 import os
 import time
 import logging
+from datetime import datetime
 
 class VideoUpdater:
     def __init__(self, notifier):
@@ -13,6 +14,6 @@ class VideoUpdater:
                 mtime = os.path.getmtime(file_name)
                 if mtime < now - 5 * 60:
                     logging.info('file "%s" is older than 5 minutes. Sending video.')
-                    self._notifier.send_recorded_video(os.path.join(video_dir, file_name))
+                    self._notifier.send_recorded_video(os.path.join(video_dir, file_name), event_time=datetime.fromtimestamp(mtime))
                 else:
                     logging.info('file "%s" is newer than 5 minutes. Not sending video now.')
